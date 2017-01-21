d13a6c7603907fa126adfcbe128a58b89dd8f029
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-01-21 01:09:13 +0100

    [tx-robot] updated from transifex

---
+++ l10n/nn_NO.js
@@ -0,0 +1,19 @@
+OC.L10N.register(
+    "files_paperhive",
+    {
+    "This file is too big to be opened. Please download the file instead." : "Fila er for stor for å opnast. Ver venleg å laste ned fila i staden for.",
+    "Cannot read the file." : "Kan ikkje lesa fila.",
+    "Invalid file path supplied." : "Ugyldig bane for fil er oppgjett.",
+    "The file is locked." : "Fila er låst.",
+    "An internal server error occurred." : "Ein intern feil oppstod!",
+    "Problem connecting to PaperHive." : "Problem ved tilkopling til PaperHive.",
+    "The file already exists." : "Fila finst alt.",
+    "Received wrong response from PaperHive." : "Mottok feil svar frå PaperHive.",
+    "Show Paper" : "Vis papir.",
+    "An error occurred!" : "Ein feil oppstod!",
+    "Discuss" : "Diskuter",
+    "PaperHive Book" : "PaperHive bok",
+    "Your Book ID" : "Din bok ID",
+    "Saving.." : "Lagrar..."
+},
+"nplurals=2; plural=(n != 1);");
