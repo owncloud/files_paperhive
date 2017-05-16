4bf450aba7839b99cf500fbc1257685a69101c47
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-05-17 01:11:30 +0200

    [tx-robot] updated from transifex

---
+++ l10n/is.js
@@ -0,0 +1,27 @@
+OC.L10N.register(
+    "files_paperhive",
+    {
+    "This file is too big to be opened. Please download the file instead." : "Þessi skrá er of stór til að opna hana hér. Halaðu henni niður í staðinn.",
+    "Cannot read the file." : "Get ekki lesið skrána.",
+    "Invalid file path supplied." : "Ógild skráarslóð.",
+    "The file is locked." : "Skráin er læst.",
+    "An internal server error occurred." : "Innri villa kom upp.",
+    "Problem connecting to PaperHive." : "Villa við tengingu við PaperHive.",
+    "The file already exists." : "Skráin er nú þegar til.",
+    "Received wrong response from PaperHive." : "Rangt svar barst frá PaperHive.",
+    "Show Paper" : "Birta skjal",
+    "Visit PaperHive at" : "Heimsæktu PaperHive á",
+    "and transform reading into a process of collaboration!" : "og breyttu lestri yfir í samstafsverkefni!",
+    "Your DocID" : "DocID-auðkennið þitt",
+    "is the last fragment of PaperHive document URL." : "er síðasti hluti PaperHive-skjalaslóðarinnar.",
+    "Exemplary URL" : "Dæmi um URL-slóð",
+    "An error occurred!" : "Villa kom upp!",
+    "Discuss" : "Ræða",
+    "Your [.paperhive] file is not a valid PaperHive document, please redownload document" : "[.paperhive] skráin þín er ekki gilt PaperHive-skjal, þú ættir að sækja skjalið aftur",
+    "PaperHive" : "PaperHive",
+    "URL or DocID" : "Slóð eða DocID",
+    "Saving..." : "Er að vista ...",
+    "Please reload the page, error occured" : "Þú ættir að hlaða síðunni aftur inn, villa kom upp",
+    "Cannot add your PaperHive document of ID {id}. File exists or error connecting to PaperHive" : "Get ekki bætt við PaperHive-skjalinu þínu með auðkennið {id}. Skráin er fyrirliggjandi eða að villa kom upp í tengingu við PaperHive"
+},
+"nplurals=2; plural=(n % 10 != 1 || n % 100 == 11);");
