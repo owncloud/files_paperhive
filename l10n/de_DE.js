f480557e3f6fdb0fb7a5d842e6990e190e5f0639
ownClouders <devops@owncloud.com>
2019-03-14 05:02:15 +0000

    [tx] updated from transifex

--- l10n/de_DE.js
+++ l10n/de_DE.js
@@ -1,14 +1,6 @@
 OC.L10N.register(
     "files_paperhive",
     {
-    "This file is too big to be opened. Please download the file instead." : "Die Datei ist zu groß zum öffnen. Bitte laden Sie stattdessen die Datei runter.",
-    "Cannot read the file." : "Datei nicht lesbar.",
-    "Invalid file path supplied." : "Ungültiger Dateipfad angegeben.",
-    "The file is locked." : "Die Datei ist gesperrt.",
-    "An internal server error occurred." : "Ein interner Serverfehler ist aufgetreten.",
-    "Problem connecting to PaperHive." : "Problem beim Verbinden zu PaperHive",
-    "The file already exists." : "Die Datei existiert bereits.",
-    "Received wrong response from PaperHive." : "Falsche Rückmeldung von PaperHive erhalten.",
     "Show Paper" : "Papier anzeigen",
     "Visit PaperHive at" : "PaperHive besuchen auf",
     "and transform reading into a process of collaboration!" : "und ändern Sie das Lesen in einen Prozess der Zusammenarbeit!",
@@ -17,11 +9,18 @@ OC.L10N.register(
     "Exemplary URL" : "Exemplarische URL",
     "An error occurred!" : "Ein Fehler ist aufgetreten!",
     "Discuss" : "Diskutieren",
-    "Your [.paperhive] file is not a valid PaperHive document, please redownload document" : "Ihre [.paperhive] Datei ist kein gültiges PaperHive-Dokument, bitte laden Sie das Dokument erneut herunter",
+    "Would you like to open in new window?" : "Möchten Sie ein neues Fenster öffnen?",
     "PaperHive" : "PaperHive",
-    "URL or DocID" : "URL oder DocID",
+    "DocID" : "DocID",
     "Saving..." : "Speichervorgang…",
     "Please reload the page, error occured" : "Bitte Seite neuladen, ein Fehler ist aufgetreten",
-    "Cannot add your PaperHive document of ID {id}. File exists or error connecting to PaperHive" : "Ihr PaperHive-Dokument mit der ID {id} konnte nicht hinzugefügt werden. Datei existiert bereits oder ein Fehler beim Verbinden mit PaperHive."
+    "Cannot add your PaperHive document with BookID {id}. {message}" : "Ihr PaperHive-Dokument mit der BookID {id} konnte nicht hinzugefügt werden. {message}",
+    "No such document found in database." : "Dokument nicht in Datenbank gefunden.",
+    "Problem connecting to PaperHive." : "Problem beim Verbinden zu PaperHive",
+    "Received wrong response from PaperHive." : "Falsche Rückmeldung von PaperHive erhalten.",
+    "Problem connecting to PaperHive to fetch discussions." : "Problem beim Verbinden zu PaperHive, um Diskussion zu laden.",
+    "The file already exists." : "Die Datei existiert bereits.",
+    "The file is locked." : "Die Datei ist gesperrt.",
+    "An internal server error occurred." : "Ein interner Serverfehler ist aufgetreten."
 },
 "nplurals=2; plural=(n != 1);");
