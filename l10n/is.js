587f2b3f17630739a1e431a8056ceb7600ba1670
ownClouders <devops@owncloud.com>
2019-04-12 05:01:07 +0000

    [tx] updated from transifex

--- l10n/is.js
+++ l10n/is.js
@@ -1,14 +1,6 @@
 OC.L10N.register(
     "files_paperhive",
     {
-    "This file is too big to be opened. Please download the file instead." : "Þessi skrá er of stór til að opna hana hér. Halaðu henni niður í staðinn.",
-    "Cannot read the file." : "Get ekki lesið skrána.",
-    "Invalid file path supplied." : "Ógild skráarslóð.",
-    "The file is locked." : "Skráin er læst.",
-    "An internal server error occurred." : "Innri villa kom upp.",
-    "Problem connecting to PaperHive." : "Villa við tengingu við PaperHive.",
-    "The file already exists." : "Skráin er nú þegar til.",
-    "Received wrong response from PaperHive." : "Rangt svar barst frá PaperHive.",
     "Show Paper" : "Birta skjal",
     "Visit PaperHive at" : "Heimsæktu PaperHive á",
     "and transform reading into a process of collaboration!" : "og breyttu lestri yfir í samstafsverkefni!",
@@ -17,11 +9,18 @@ OC.L10N.register(
     "Exemplary URL" : "Dæmi um URL-slóð",
     "An error occurred!" : "Villa kom upp!",
     "Discuss" : "Ræða",
-    "Your [.paperhive] file is not a valid PaperHive document, please redownload document" : "[.paperhive] skráin þín er ekki gilt PaperHive-skjal, þú ættir að sækja skjalið aftur",
+    "Would you like to open in new window?" : "myndirðu vilja opna í nýjum glugga?",
     "PaperHive" : "PaperHive",
-    "URL or DocID" : "Slóð eða DocID",
+    "DocID" : "DocID-auðkenni",
     "Saving..." : "Er að vista ...",
     "Please reload the page, error occured" : "Þú ættir að hlaða síðunni aftur inn, villa kom upp",
-    "Cannot add your PaperHive document of ID {id}. File exists or error connecting to PaperHive" : "Get ekki bætt við PaperHive-skjalinu þínu með auðkennið {id}. Skráin er fyrirliggjandi eða að villa kom upp í tengingu við PaperHive"
+    "Cannot add your PaperHive document with BookID {id}. {message}" : "Get ekki bætt við PaperHive-skjalinu þínu með BookID auðkennið {id}. {message}",
+    "Problem connecting to PaperHive." : "Villa við tengingu við PaperHive.",
+    "Received wrong response from PaperHive." : "Rangt svar barst frá PaperHive.",
+    "The file already exists." : "Skráin er nú þegar til.",
+    "Could not save document." : "Gat ekki vistað skjalið.",
+    "Could not save document metadata." : "Gat ekki vistað lýsigögn skjalsins.",
+    "The file is locked." : "Skráin er læst.",
+    "An internal server error occurred." : "Innri villa kom upp."
 },
 "nplurals=2; plural=(n % 10 != 1 || n % 100 == 11);");
