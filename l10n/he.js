3f9eecb428467fde1b65dfab35ef83a86bff1b7c
ownClouders <devops@owncloud.com>
2019-05-21 05:01:10 +0000

    [tx] updated from transifex

--- l10n/he.js
+++ l10n/he.js
@@ -1,14 +1,6 @@
 OC.L10N.register(
     "files_paperhive",
     {
-    "This file is too big to be opened. Please download the file instead." : "קובץ זה גדול מדי לפתיחה. יש להוריד את הקובץ במקום",
-    "Cannot read the file." : "לא ניתן לקרוא את הקובץ.",
-    "Invalid file path supplied." : "סופק נתיב קובץ לא חוקי.",
-    "The file is locked." : "הקובץ נעול.",
-    "An internal server error occurred." : "אירעה שגיאת שרת פנימית.",
-    "Problem connecting to PaperHive." : "בעיה בהתחברות ל- PaperHive.",
-    "The file already exists." : "הקובץ כבר קיים.",
-    "Received wrong response from PaperHive." : "התקבלה תגובה שגוייה מ- PaperHive.",
     "Show Paper" : "הראה נייר",
     "Visit PaperHive at" : "ניתן לבקר את PaperHive ב-",
     "and transform reading into a process of collaboration!" : "ולהמיר קריאה לתהליך של שיתוף פעולה!",
@@ -17,11 +9,18 @@ OC.L10N.register(
     "Exemplary URL" : "נתיב דוגמא",
     "An error occurred!" : "אירעה שגיאה!",
     "Discuss" : "דיון",
-    "Your [.paperhive] file is not a valid PaperHive document, please redownload document" : "קובץ [.paperhive] שלך אינו מסמך PaperHive תקף, יש להוריד מחדש את המסמך",
+    "Would you like to open in new window?" : "האם לפתוח בחלון חדש?",
     "PaperHive" : "PaperHive",
-    "URL or DocID" : "נתיב או DocID",
+    "DocID" : "DocID",
     "Saving..." : "שמירה…",
     "Please reload the page, error occured" : "אירעה שגיאה, יש לטעון את הדף שנית.",
-    "Cannot add your PaperHive document of ID {id}. File exists or error connecting to PaperHive" : "לא ניתן להוסיף את מסמך PaperHive שלך עם זיהוי {id}. קובץ קיים או שגיאה בחיבור ל- PaperHive"
+    "No such document found in database." : "מסמך זה לא נמצא במסד הנתונים.",
+    "Problem connecting to PaperHive." : "בעיה בהתחברות ל- PaperHive.",
+    "Received wrong response from PaperHive." : "התקבלה תגובה שגוייה מ- PaperHive.",
+    "Document with this BookID cannot be found" : "מסמך עם BookID זה לא נמצא",
+    "The file already exists." : "הקובץ כבר קיים.",
+    "Could not save document." : "לא ניתן היה לשמור את המסמך.",
+    "The file is locked." : "הקובץ נעול.",
+    "An internal server error occurred." : "אירעה שגיאת שרת פנימית."
 },
 "nplurals=4; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;");
