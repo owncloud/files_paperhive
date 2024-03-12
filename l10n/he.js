1218d8c98596d7d72f235d4e4ff2be04a4a1d6e9
ownClouders <devops@owncloud.com>
2024-03-12 07:43:10 +0000

    [tx] updated from transifex

--- l10n/he.js
+++ l10n/he.js
@@ -26,4 +26,4 @@ OC.L10N.register(
     "The file is locked." : "הקובץ נעול.",
     "An internal server error occurred." : "אירעה שגיאת שרת פנימית."
 },
-"nplurals=4; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;");
+"nplurals=3; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: 2;");
